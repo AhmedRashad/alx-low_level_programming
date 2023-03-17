ce6cf51d47a7a49df3fa5174db1140f423082ac2
AhmedRashad <ahmedrabie441@gmail.com>
2023-03-18 01:46:20 +0200

    .

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,42 +6,20 @@
  */
 int main(void)
 {
-	int count1 = (int)'0';
-	int count2 = count1;
-	int count3 = count1;
-	int count4 = count1 + 1;
-	int count_stop = (int)'9';
+	int num;
 
-	while (count1 <= count_stop)
+	for (num = 0; num <= 9; num++)
 	{
-		while (count2 <= count_stop)
-		{
-			while (count3 <= count_stop)
-			{
-				while (count4 <= count_stop)
-				{
-					putchar((char)count1);
-					putchar((char)count2);
-					putchar(' ');
-					putchar((char)count3);
-					putchar((char)count4);
-					putchar(',');
-					putchar(' ');
-					count4++;
-				}
-				count3++;
-				count4 = (int)'0';
-			}
-			count2++;
-			count3 = count1;
-			count4 = count2 + 1;
-		}
-		count1++;
-		count2 = (int)'0';
-		count3 = count1;
-		count4 = count2 + 1;
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
-	putchar((char)10);
+
+	putchar('\n');
+
 	return (0);
 }
 
