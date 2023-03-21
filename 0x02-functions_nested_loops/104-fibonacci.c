bc7dbfac4760365f889d4f6c0746a38dbee27476
AhmedRashad <ahmedrabie441@gmail.com>
2023-03-22 01:35:54 +0200

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,28 @@
 
 int main(void)
 {
-	long double num1 = 0, num2 = 0, temp = 0;
-	int i;
-
-	for (i = 1; i <= 98; i++)
+	unsigned long int i, j, k, j1, j2, k1, k2;
+	j = 1;
+	k = 2;
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
-		if (i > 2)
-		{
-			temp = num1;
-			num1 += num2;
-			num2 = temp;
-		}
-		else
-		{
-			num1 = i;
-			num2 = i - 1;
-		}
-		printf("%.0Lf", num1);
-		if (i == 98)
-			continue;
-		printf(", ");
+	printf(", %lu", k);
+	k = k + j;
+	j = k - j;
+	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+	printf(", %lu", k1 + (k2 / 1000000000));
+	printf("%lu", k2 % 1000000000);
+	k1 = k1 + j1;
+	j1 = k1 - j1;
+	k2 = k2 + j2;
+	j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
